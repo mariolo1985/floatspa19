eea7d98bfd4576b969e49bc62b61cb1a21dc1b14
mariolo1985 <loserpunkx182@gmail.com>
2018-05-11 08:29:22 -0600

    immediate calls

--- js/massageservices.js
+++ js/massageservices.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import MassageServices from '../build/Sections/massageServices';
 
-window.onload = () => {
+(() => {
     render(
         <MassageServices />
         ,
         document.getElementById('services-benefits-container')
     );
-}
+})();
